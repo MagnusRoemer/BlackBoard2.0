a2af322bb74b9ec8ad51d3f7d7931143e3e450ab
Nickolai <nipet18@student.sdu.dk>
2020-05-02 22:46:27 +0100

    min. CSS more jQuery + content

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    $('.flex-container').load('Content.html', function () {
+        $('.full-text').hide()
+
+        $('.card').click(function () {
+            $(this).children().toggle();
+            $(this).parent().siblings().toggle();
+        });
+    });
+});
